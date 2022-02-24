37ba3183ba7e96ecf33ee4ce90f4a5cadb981d7f
krupa3910 <landofcanada@outlook.com>
2022-02-23 20:08:00 -0500

    fourth commit

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get('/', function(req, res, next) {
 });
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home'});
+  res.render('home', { title: 'Home'});
 });
 
 /* GET About Me page. */
